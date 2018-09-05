14cc658f501c636e42825b039d428fc31b6ac0f8
Spencer Carli <spencercarli@gmail.com>
2018-09-05 09:38:54 -0700

    Bring the double tap logic into its own component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 
+import DoubleTap from './DoubleTap';
+
 const w = Dimensions.get('window');
 
 export default class App extends React.Component {
@@ -10,28 +12,16 @@ export default class App extends React.Component {
 
   toggleLike = () => this.setState(state => ({ liked: !state.liked }));
 
-  // https://gist.github.com/brunotavares/3c9a373ba5cd1b4ff28b
-  lastDoubleTap = null;
-  DOUBLE_PRESS_DELAY = 300;
-  handleDoubleTap = () => {
-    const now = Date.now();
-    if (this.lastDoubleTap && (now - this.lastDoubleTap) < this.DOUBLE_PRESS_DELAY) {
-      this.toggleLike();
-    } else {
-      this.lastDoubleTap = now;
-    }
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <TouchableWithoutFeedback onPress={this.handleDoubleTap}>
+        <DoubleTap onDoubleTap={this.toggleLike}>
           <Image
             source={{ uri: `https://images.pexels.com/photos/671557/pexels-photo-671557.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=${w.width}` }}
             style={{ width: w.width, height: w.width }}
             resizeMode="cover"
           />
-        </TouchableWithoutFeedback>
+        </DoubleTap>
         <View style={styles.iconRow}>
           <TouchableOpacity onPress={this.toggleLike}>
             <Image
