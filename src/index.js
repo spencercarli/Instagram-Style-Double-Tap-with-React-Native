e94c6240e45012a4b6bf966bedf4b1a97a575512
Spencer Carli <spencercarli@gmail.com>
2018-09-05 09:31:41 -0700

    Double tap implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 
 const w = Dimensions.get('window');
 
@@ -10,14 +10,28 @@ export default class App extends React.Component {
 
   toggleLike = () => this.setState(state => ({ liked: !state.liked }));
 
+  // https://gist.github.com/brunotavares/3c9a373ba5cd1b4ff28b
+  lastDoubleTap = null;
+  DOUBLE_PRESS_DELAY = 300;
+  handleDoubleTap = () => {
+    const now = Date.now();
+    if (this.lastDoubleTap && (now - this.lastDoubleTap) < this.DOUBLE_PRESS_DELAY) {
+      this.toggleLike();
+    } else {
+      this.lastDoubleTap = now;
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          source={{ uri: `https://images.pexels.com/photos/671557/pexels-photo-671557.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=${w.width}` }}
-          style={{ width: w.width, height: w.width }}
-          resizeMode="cover"
-        />
+        <TouchableWithoutFeedback onPress={this.handleDoubleTap}>
+          <Image
+            source={{ uri: `https://images.pexels.com/photos/671557/pexels-photo-671557.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=${w.width}` }}
+            style={{ width: w.width, height: w.width }}
+            resizeMode="cover"
+          />
+        </TouchableWithoutFeedback>
         <View style={styles.iconRow}>
           <TouchableOpacity onPress={this.toggleLike}>
             <Image
